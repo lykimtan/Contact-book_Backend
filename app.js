d3478c27f1c5d5e0a522beaa20767cca7622d3ba
Kim Tan <tanb2205905@student.ctu.edu.vn>
2025-06-06 10:56:38 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Start update
+const contactRouter = require('./app/routes/contact.route'); 
+
+app.use('/api/contacts', contactRouter)
+
+//end update
+
 app.get('/', (req, res) => {
     res.json({ Message : "Welcome to Contact Book Application"})
 });
