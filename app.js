fa65fc3768a875cab01d3b22b190399f89a3dcd8
Kim Tan <tanb2205905@student.ctu.edu.vn>
2025-06-06 11:14:19 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ApiError = require("./app/api-error")
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 //Start update
 const contactRouter = require('./app/routes/contact.route'); 
 
+
 app.use('/api/contacts', contactRouter)
 
 //end update
@@ -17,4 +19,16 @@ app.get('/', (req, res) => {
     res.json({ Message : "Welcome to Contact Book Application"})
 });
 
+//handle 404 response
+app.use((req, res, next) => {
+    //thuc thi khi khong co route nao duoc dinh nghia 
+    return next(new ApiError(404, "Resource not found"))
+})
+
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    })
+})
 module.exports = app;
